6a331c8af44d4631d12ac3a5fdbf6286eead1714
Darya Skobkareva <daryaskobkareva@yandex.ru>
2023-07-04 19:41:34 +0400

    Добавлена параметризация для кнопок "Заказать", удалён дублирующий тестовый метод

--- src/test/java/ru/yandex/praktikum/selenium/OrderTest.java
+++ src/test/java/ru/yandex/praktikum/selenium/OrderTest.java
@@ -7,12 +7,14 @@ import ru.yandex.praktikum.selenium.step.OrderScooterStep;
 
 @RunWith(Parameterized.class)
 public class OrderTest extends BaseTest {
+    private final boolean isTopButton;
     private final String name;
     private final String lastName;
     private final String address;
     private final String phoneNumber;
     private final String date;
-    public OrderTest(String name, String lastName, String address, String phoneNumber, String date) {
+    public OrderTest(boolean isTopButton, String name, String lastName, String address, String phoneNumber, String date) {
+        this.isTopButton = isTopButton;
         this.name = name;
         this.lastName = lastName;
         this.address = address;
@@ -22,19 +24,17 @@ public class OrderTest extends BaseTest {
     @Parameterized.Parameters
     public static Object[][] createOrder() {
         return new Object[][] {
-                {"Валентин", "Серов", "Лаврушинский пер., 10", "79918651911", "15.07.2023"},
-                {"Казимир", "Малевич", "Крымский вал, 10", "+79918791935", "19.07.2023"},
+                {true, "Валентин", "Серов", "Лаврушинский пер., 10", "79918651911", "15.07.2023"},
+                {false, "Казимир", "Малевич", "Крымский вал, 10", "+79918791935", "19.07.2023"},
         };
     }
     @Test
-    public void checkOrderingScooterByTopButton() {
-        mainPage.clickOrderTopButtonOnMainPage();
-        OrderScooterStep orderScooterStep = new OrderScooterStep(webDriver);
-        orderScooterStep.orderScooter(name, lastName, address, phoneNumber, date);
-    }
-    @Test
-    public void checkOrderingScooterByBottomButton() {
-        mainPage.clickOrderBottomButtonOnMainPage();
+    public void checkOrderScooter() {
+        if(isTopButton) {
+            mainPage.clickOrderTopButtonOnMainPage();
+        } else {
+            mainPage.clickOrderBottomButtonOnMainPage();
+        }
         OrderScooterStep orderScooterStep = new OrderScooterStep(webDriver);
         orderScooterStep.orderScooter(name, lastName, address, phoneNumber, date);
     }
